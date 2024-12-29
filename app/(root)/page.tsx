4a8fd973f32d764264cffa762eb294ce2473d693
William Nguyen <nguyennamquoc.mar@gmail.com>
2024-12-29 14:59:55 -0500

    set up basic routes (root, auth, transformation) and integrate clerk authentication for user management

---
+++ app/(root)/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { UserButton } from '@clerk/nextjs'
+
+const Home = () => {
+  return (
+    <div>
+      <p>Home</p>
+      <UserButton />
+      
+    </div>
+  )
+}
+
+export default Home
